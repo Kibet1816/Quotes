a6f6d683d4e975f5da299d6deb2101dcc5a775cf
Denis Kibet <kibet1816@gmail.com>
2019-06-07 12:03:02 +0300

    Add button functions

--- src/app/quotes-details/quotes-details.component.ts
+++ src/app/quotes-details/quotes-details.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
-  selector: 'app-quotes-details',
-  templateUrl: './quotes-details.component.html',
-  styleUrls: ['./quotes-details.component.css']
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
 })
-export class QuotesDetailsComponent implements OnInit {
+export class QuoteDetailsComponent implements OnInit {
 
+  @Input()quote: Quote;
+  @Output()isComplete = new EventEmitter<boolean>();
+
+  upvote = 0;
+  downvote = 0;
+  upVote() {
+     this.upvote = this.upvote + 1;
+  }
+  downVote() {
+    this.downvote = this.downvote + 1;
+    }
+
+  deleteQuote(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
   constructor() { }
 
   ngOnInit() {
