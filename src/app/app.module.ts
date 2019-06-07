6dc0ae9df05b50e805455696437770ebbffc51c9
Denis Kibet <kibet1816@gmail.com>
2019-06-07 11:47:46 +0300

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuotesComponent } from './quotes/quotes.component';
+import { QuotesFormComponent } from './quotes-form/quotes-form.component';
+import { QuotesDetailsComponent } from './quotes-details/quotes-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuotesComponent,
+    QuotesFormComponent,
+    QuotesDetailsComponent
   ],
   imports: [
     BrowserModule
